f2a4610adf829fc750a86ff2ff07eb8adf403e4b
jerzonhr <106408622+jerzonhr@users.noreply.github.com>
2022-10-11 13:42:38 -0600

    📦 NEW: exercise3.1-3.2 ~ 0.5h

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express() // creates an express app which is a http server
+app.use(express.json()) //request body parser
+
+let persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+]
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello there!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+	res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
