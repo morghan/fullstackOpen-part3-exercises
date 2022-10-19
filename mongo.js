b951014442d9b191907549205bcee96853c4eabd
morghan <joel.hz24@gmail.com>
2022-10-19 16:17:42 -0600

    🐛 FIX: Arguments validation

--- mongo.js
+++ mongo.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
+if (process.argv.length < 3) {
+	console.log(
+		'Please provide the password as an argument: node mongo.js <password>'
+	)
+	process.exit(1)
+}
+
 const username = 'fullstack'
 let password = process.argv[2]
 const url = `mongodb+srv://${username}:${password}@fullstackopen.gdxcsye.mongodb.net/phonebook?retryWrites=true&w=majority`
