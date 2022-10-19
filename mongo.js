d555ea803bf61e992e7c2744229d4e86af7fba89
morghan <joel.hz24@gmail.com>
2022-10-19 16:21:06 -0600

    🐛 FIX: mongoose model

--- mongo.js
+++ mongo.js
@@ -16,7 +16,7 @@ const personSchema = new mongoose.Schema({
 	number: String,
 })
 
-const Person = new mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
 	mongoose.connect(url).then((result) => {
