8bffd49223151ac5442af1d684172798e32fabc9
morghan <joel.hz24@gmail.com>
2022-10-19 16:15:08 -0600

    👌 IMPROVE: added logs

--- mongo.js
+++ mongo.js
@@ -13,12 +13,14 @@ const Person = new mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
 	mongoose.connect(url).then((result) => {
+		console.log('connected')
 		Person.find({}).then((result) => {
 			console.log('Phonebook:')
 			result.forEach((person) => {
 				console.log(`${person.name} - ${person.number}`)
 			})
 			mongoose.connection.close()
+			console.log('connection closed')
 		})
 	})
 }
@@ -27,9 +29,11 @@ if (process.argv.length === 5) {
 	const number = process.argv[4].trim()
 	const person = new Person({ name, number })
 	mongoose.connect(url).then((result) => {
+		console.log('connected')
 		person.save().then(() => {
 			console.log(`added ${name} number: ${number} to phonebook`)
 			mongoose.connection.close()
+			console.log('connection closed')
 		})
 	})
 }
